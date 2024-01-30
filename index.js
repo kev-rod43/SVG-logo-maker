2b3694047790705105d3373646bdd1d04b346585
Kev-rod43 <kev.rod43@yahoo.com>
2024-01-30 13:08:05 -0500

    finished the Shapes module, and imported the module into index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const shapes = require('./lib/shapes.js');
+const {Shapes, Circle, Square, Triangle} = require('./lib/shapes.js');
 
 const questions = [
     {
